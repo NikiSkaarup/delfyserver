2b6ec55d75574ee8aa8e43485eb40cfe4f0b4cd5
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-09-14 14:07:59 +0200

    ESLINTing

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const wss = new WebSocket.Server({ port: 8001 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
         wss.clients.forEach(function each(client) {
-            if(client == ws) return;
+            if(client === ws) return;
             if (client.readyState === WebSocket.OPEN) {
                 client.send(data);
             }
-            console.log(data)
+            console.log(data);
         });
     });
 
