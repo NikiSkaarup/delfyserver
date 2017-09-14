4d5ad7fc114de232ce908cbc03f85467c4a945f8
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-09-14 14:09:39 +0200

    Formatting

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const wss = new WebSocket.Server({ port: 8001 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
         wss.clients.forEach(function each(client) {
-            if(client === ws) return;
+            if (client === ws) return;
             if (client.readyState === WebSocket.OPEN) {
                 client.send(data);
             }
